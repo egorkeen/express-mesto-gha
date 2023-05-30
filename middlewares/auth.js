c1c34a4d3ccfcb58941c1337e5aa0e5974087773
Egor Kirienko <egor.keen@mail.ru>
2023-05-30 19:37:04 +0300

    14.4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return new AuthorizeError('Неправильные почта или пароль');
+    return next(new AuthorizeError('Неправильные почта или пароль'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return new AuthorizeError('Неправильные почта или пароль');
+    return next(new AuthorizeError('Неправильные почта или пароль'));
   }
 
   req.user = payload;
