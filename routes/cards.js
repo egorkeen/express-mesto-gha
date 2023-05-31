67dfd505b8bc7d855a24779b2ed7c7478fc6cb83
Egor Kirienko <egor.keen@mail.ru>
2023-05-31 15:29:39 +0300

    14.7

--- routes/cards.js
+++ routes/cards.js
@@ -6,11 +6,12 @@ const {
   likeCard,
   dislikeCard,
 } = require('../controllers/cards');
+const auth = require('../middlewares/auth');
 
-cardsRouter.get('/cards', getCards);
-cardsRouter.delete('/cards/:cardId', deleteCard);
-cardsRouter.post('/cards', postCard);
-cardsRouter.put('/cards/:cardId/likes', likeCard);
-cardsRouter.delete('/cards/:cardId/likes', dislikeCard);
+cardsRouter.get('/cards', auth, getCards);
+cardsRouter.delete('/cards/:cardId', auth, deleteCard);
+cardsRouter.post('/cards', auth, postCard);
+cardsRouter.put('/cards/:cardId/likes', auth, likeCard);
+cardsRouter.delete('/cards/:cardId/likes', auth, dislikeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,12 @@ const {
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
-usersRouter.get('/users', getUsers);
-usersRouter.get('/users/me', getUser);
-usersRouter.get('/users/:userId', getUserById);
-usersRouter.patch('/users/me', updateProfile);
-usersRouter.patch('/users/me/avatar', updateAvatar);
+usersRouter.get('/users', auth, getUsers);
+usersRouter.get('/users/me', auth, getUser);
+usersRouter.get('/users/:userId', auth, getUserById);
+usersRouter.patch('/users/me', auth, updateProfile);
+usersRouter.patch('/users/me/avatar', auth, updateAvatar);
 
 module.exports = usersRouter;
