451a31bdc8fba14cf9672a5c20fcd0522725d399
Egor Kirienko <egor.keen@mail.ru>
2023-06-03 21:29:47 +0300

    fix 11

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const AuthorizeError = require('../errors/AuthorizeError');
 
 module.exports = (req, res, next) => {
-  const authorization = req.headers;
+  const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new AuthorizeError('Неверные почта или пароль'));
--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,7 +1,8 @@
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
     res.status(500).send({ message: 'Произошла ошибка на сервере' });
   }
+  next();
 };
