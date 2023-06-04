aa039b7eb3831faf83c9a4774e40b79eb8827a6a
Egor Kirienko <egor.keen@mail.ru>
2023-06-04 15:31:35 +0300

    hotfix

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const { URL_REGEX } = require('../utils/constants');
-const AuthorizeError = require('../errors/AuthorizeError');
+const AuthorizeError = require('../errors/AuthorizeError')
 
 // схема пользователя
 const userSchema = mongoose.Schema({
@@ -48,13 +48,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('password')
     .then((user) => {
       if (!user) {
-        throw new AuthorizeError('Неправильные почта или пароль');
+        return Promise.reject(new AuthorizeError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            throw new AuthorizeError('Неправильные почта или пароль');
+            return Promise.reject(new AuthorizeError('Неправильные почта или пароль'));
           }
 
           return user;
